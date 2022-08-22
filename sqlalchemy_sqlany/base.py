6dc5e4ee7da969fcbdd87f894b43a22e5af05221
James Russo <jr@halo3.net>
2022-08-22 12:16:16 -0400

    set statement cache to false to prevent warning.

--- sqlalchemy_sqlany/base.py
+++ sqlalchemy_sqlany/base.py
@@ -412,6 +412,7 @@ class SQLAnyDialect(default.DefaultDialect):
     supports_unicode_statements = False
     supports_sane_rowcount = False
     supports_sane_multi_rowcount = False
+    supports_statement_cache = False
 
     supports_native_boolean = False
     supports_unicode_binds = False
